b89a394f80be860f3c979d32a76eaf76255e6847
Alan <malongalan@gmail.com>
2021-05-07 13:10:29 +0200

    time conversion done

--- code-in-game/src/main/java/Solution.java
+++ code-in-game/src/main/java/Solution.java
@@ -1,21 +1,49 @@
 import java.math.BigInteger;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Solution {
-	
-	//Combinaison de n dans p
+
+	public static void main(String[] args) {
+		String a = timeConversion("07:05:45PM");
+	}
+
+	// Combinaison de n dans p
 	static BigInteger combinaison(int n, int p) {
-		return factorial(n).divide(factorial(p).multiply(factorial(n-p)));
-	} 
+		return factorial(n).divide(factorial(p).multiply(factorial(n - p)));
+	}
+
+	public static BigInteger factorial(int n) {
+		BigInteger f = new BigInteger("1");
+
+		for (int i = 2; i <= n; i++)
+			f = f.multiply(BigInteger.valueOf(i));
+
+		return f;
+	}
+
+	// conversion de date
+
+	public static String timeConversion(String s) {
+		Date date = null;
+		String output = null;
+
+		DateFormat df = new SimpleDateFormat("hh:mm:ssaa");
+		DateFormat outputformat = new SimpleDateFormat("HH:mm:ss");
+
+		try {
+			date = df.parse(s);
+
+			output = outputformat.format(date);
+		} catch (ParseException pe) {
+			pe.printStackTrace();
+		}
+
+		return output;
+	}
+
 	
-	 public static BigInteger factorial(int n) {
-	    BigInteger f = new BigInteger("1");
-	  
-	    for (int i = 2; i <= n; i++) 
-	    f = f.multiply(BigInteger.valueOf(i));
-	    
-	    return f;
-	 } 
-	 
-	 
 
 }
